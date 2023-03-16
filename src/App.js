65a45dce4b80536d73803eec1ffda375c2ff66d0
Spartan <spartan@IMS-109MBA.local>
2023-03-16 15:16:53 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit heyyy<code>src/App.js</code> hii and save to reload.
+        </p>
+
+        <p>
+          hii how are you
+        </p>
+        <p>
+
+          byeeee
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
